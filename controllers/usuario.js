63b67b681f3ddf4410220022799f78fe16852cc9
venonh2 <brendow-evan@hotmail.com>
2019-11-25 19:25:32 -0300

    adicionando view sinopse

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -34,7 +34,7 @@ module.exports = function (app) {
 
                                     newUser.save()
                                         .then(() => {
-                                            res.json({success: true, message: 'Usuario criado com sucesso', statusCode: 201 })
+                                            //res.json({success: true, message: 'Usuario criado com sucesso', statusCode: 201 })
                                             res.render('usuario/login');
                                         })
                                         .catch(err => res.json({success: false, message: err, statusCode:500}));
